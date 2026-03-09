c4c36a05413ad51346c509207a4d7742d7279094
aleixruf <aleix.ruf01@estudiant.upf.edu>
2026-03-09 15:46:03 +0100

    GUI.py cleaned

--- GUI.py
+++ GUI.py
@@ -407,7 +407,7 @@ class FrameBP:
             "borderwidth": 1
         }
 
-        # Row 1 (Top) - rely=0.25
+        # Row 1 (Top)
         self.ST_stat11_label = tk.Label(self.ST_unistats_label)
         self.ST_stat11_label.place(relx=0.125, rely=0.25, anchor="center", relwidth=0.24, relheight=0.45)
         self.ST_stat11_label.configure(**stat_config, text='''Ranking''')
@@ -424,7 +424,7 @@ class FrameBP:
         self.ST_stat14_label.place(relx=0.875, rely=0.25, anchor="center", relwidth=0.24, relheight=0.45)
         self.ST_stat14_label.configure(**stat_config, text='''Available spots''')
 
-        # Row 2 (Bottom) - rely=0.75
+        # Row 2 (Bottom)
         self.ST_stat21_label = tk.Label(self.ST_unistats_label)
         self.ST_stat21_label.place(relx=0.125, rely=0.75, anchor="center", relwidth=0.24, relheight=0.45)
         self.ST_stat21_label.configure(**stat_config, text='''Engineering\nRanking''')
@@ -464,7 +464,7 @@ class FrameBP:
         self.ST_web_button.configure(text='''Website''')
         self.ST_web_button.configure(compound='left')
         
-        # Hide stats frame initially
+        # hide stats frame initially
         self.ST_facts_frame.place_forget()
 
 #___________________________________________________________LOGIN PAGE__________________________________________________________
@@ -975,116 +975,6 @@ class FrameBP:
                 foreground="white", highlightbackground=self.THEME["BG_GREY"],
                 highlightcolor=self.THEME["TEXT_DARK"], text='''Log out''')
 
-#__________________________________________________________________________________________________________________________________
-# Code related to scroll and special widgets, will be implemented later.
-
-class AutoScroll(object):
-    '''Configure the scrollbars for a widget.'''
-    def __init__(self, master):
-        #  Rozen. Added the try-except clauses so that this class
-        #  could be used for scrolled entry widget for which vertical
-        #  scrolling is not supported. 5/7/14.
-        try:
-            vsb = ttk.Scrollbar(master, orient='vertical', command=self.yview)
-        except:
-            pass
-        hsb = ttk.Scrollbar(master, orient='horizontal', command=self.xview)
-        try:
-            self.configure(yscrollcommand=self._autoscroll(vsb))
-        except:
-            pass
-        self.configure(xscrollcommand=self._autoscroll(hsb))
-        self.grid(column=0, row=0, sticky='nsew')
-        try:
-            vsb.grid(column=1, row=0, sticky='ns')
-        except:
-            pass
-        hsb.grid(column=0, row=1, sticky='ew')
-        master.grid_columnconfigure(0, weight=1)
-        master.grid_rowconfigure(0, weight=1)
-        # Copy geometry methods of master  (taken from ScrolledText.py)
-        methods = tk.Pack.__dict__.keys() | tk.Grid.__dict__.keys() \
-                  | tk.Place.__dict__.keys()
-        for meth in methods:
-            if meth[0] != '_' and meth not in ('config', 'configure'):
-                setattr(self, meth, getattr(master, meth))
-
-    @staticmethod
-    def _autoscroll(sbar):
-        '''Hide and show scrollbar as needed.'''
-        def wrapped(first, last):
-            first, last = float(first), float(last)
-            if first <= 0 and last >= 1:
-                sbar.grid_remove()
-            else:
-                sbar.grid()
-            sbar.set(first, last)
-        return wrapped
-
-    def __str__(self):
-        return str(self.master)
-
-def _create_container(func):
-    '''Creates a ttk Frame with a given master, and use this new frame to
-    place the scrollbars and the widget.'''
-    def wrapped(cls, master, **kw):
-        container = ttk.Frame(master)
-        container.bind('<Enter>', lambda e: _bound_to_mousewheel(e, container))
-        container.bind('<Leave>', lambda e: _unbound_to_mousewheel(e, container))
-        return func(cls, container, **kw)
-    return wrapped
-
-class ScrolledWindow(AutoScroll, tk.Canvas):
-    '''A standard Tkinter Canvas widget with scrollbars that will
-    automatically show/hide as needed.'''
-    @_create_container
-    def __init__(self, master, **kw):
-        tk.Canvas.__init__(self, master, **kw)
-        AutoScroll.__init__(self, master)
-
-import platform
-def _bound_to_mousewheel(event, widget):
-    child = widget.winfo_children()[0]
-    if platform.system() == 'Windows' or platform.system() == 'Darwin':
-        child.bind_all('<MouseWheel>', lambda e: _on_mousewheel(e, child))
-        child.bind_all('<Shift-MouseWheel>', lambda e: _on_shiftmouse(e, child))
-    else:
-        child.bind_all('<Button-4>', lambda e: _on_mousewheel(e, child))
-        child.bind_all('<Button-5>', lambda e: _on_mousewheel(e, child))
-        child.bind_all('<Shift-Button-4>', lambda e: _on_shiftmouse(e, child))
-        child.bind_all('<Shift-Button-5>', lambda e: _on_shiftmouse(e, child))
-
-def _unbound_to_mousewheel(event, widget):
-    if platform.system() == 'Windows' or platform.system() == 'Darwin':
-        widget.unbind_all('<MouseWheel>')
-        widget.unbind_all('<Shift-MouseWheel>')
-    else:
-        widget.unbind_all('<Button-4>')
-        widget.unbind_all('<Button-5>')
-        widget.unbind_all('<Shift-Button-4>')
-        widget.unbind_all('<Shift-Button-5>')
-
-def _on_mousewheel(event, widget):
-    if platform.system() == 'Windows':
-        widget.yview_scroll(-1*int(event.delta/120),'units')
-    elif platform.system() == 'Darwin':
-        widget.yview_scroll(-1*int(event.delta),'units')
-    else:
-        if event.num == 4:
-            widget.yview_scroll(-1, 'units')
-        elif event.num == 5:
-            widget.yview_scroll(1, 'units')
-
-def _on_shiftmouse(event, widget):
-    if platform.system() == 'Windows':
-        widget.xview_scroll(-1*int(event.delta/120), 'units')
-    elif platform.system() == 'Darwin':
-        widget.xview_scroll(-1*int(event.delta), 'units')
-    else:
-        if event.num == 4:
-            widget.xview_scroll(-1, 'units')
-        elif event.num == 5:
-            widget.xview_scroll(1, 'units')
 def start_up():
     main.main()
 
